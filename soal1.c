7dccc47f5e8334bda02d7830adf1527aa49872e7
Rio Surya Fachrudin <32932036+riosurya12@users.noreply.github.com>
2017-11-15 14:31:06 +0700

    update soal1.c
    tambah static structnya

--- soal1.c
+++ soal1.c
@@ -50,3 +50,10 @@ int main(int argc, char *argv[])
 	umask(0);
 	return fuse_main(argc, argv, &xmp_oper, NULL);
 }
+
+static struct fuse_operations xmp_oper = 
+{
+	.getattr	= xmp_getattr,
+	.readdir	= xmp_readdir,
+	.open		= xmp_open,
+};
