9d869f4c2c44c0e353f5519a53e0eac6983a64a7
Rio Surya Fachrudin <32932036+riosurya12@users.noreply.github.com>
2017-11-15 14:33:11 +0700

    update soal1.c
    tambah static xmp_read

--- soal1.c
+++ soal1.c
@@ -39,6 +39,13 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_
 		if (filler(buf, de->d_name, &st, 0))
 	break;	
 }
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+		    struct fuse_file_info *fi)
+{
+	
+}
+	
 	
 static int (*open) (const char *, struct fuse_file_info *)
 {
