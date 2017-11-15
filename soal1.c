d4f8a45f5c6752ff632534e1926183d8d80c634c
Rio Surya Fachrudin <32932036+riosurya12@users.noreply.github.com>
2017-11-15 14:27:11 +0700

    update soal1.c
    tambah int xmp_readdir

--- soal1.c
+++ soal1.c
@@ -19,7 +19,27 @@ static int xmp_getattr(const char *path, struct stat *stbuf)
 	return 0;
 }
 
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi)
+{
+	DIR *dp;
+	struct dirent *de;
 
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(path);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		if (filler(buf, de->d_name, &st, 0))
+	break;	
+}
+	
 
 int main(int argc, char *argv[])
 {
