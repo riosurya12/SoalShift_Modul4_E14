85b121f1d06ca9652910e9d0a85cccfadb1cdbc8
Rio Surya Fachrudin <32932036+riosurya12@users.noreply.github.com>
2017-11-15 14:23:14 +0700

    Add files via upload

---
+++ soal1.c
@@ -0,0 +1,28 @@
+#define FUSE_USE_VERSION 28
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <errno.h>
+#include <sys/time.h>
+
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+	int res;
+
+	res = lstat(path, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+
+
+int main(int argc, char *argv[])
+{
+	umask(0);
+	return fuse_main(argc, argv, &xmp_oper, NULL);
+}
