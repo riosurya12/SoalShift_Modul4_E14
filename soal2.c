3140ef575e6052624b9150bab5b841a1e8178001
Rio Surya Fachrudin <32932036+riosurya12@users.noreply.github.com>
2017-11-16 20:41:55 +0700

    update soal2.c
    tambah int xmp_read

--- soal2.c
+++ soal2.c
@@ -44,3 +44,22 @@ static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 	closedir(dp);
 	return 0;
 }
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+		    struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+
+	(void) fi;
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
